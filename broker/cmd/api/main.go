acb8a15d86e1bbef4e78844c58f55581c7f414d8
arshia <arshiarezagholi1212@gmail.com>
2024-10-04 15:18:34 +0330

    implemented main and create and run the server

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %v\n", webPort)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%v", webPort),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }

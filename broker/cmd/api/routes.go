bbe955bac23adc137f9c3485b1a5f5013b7aae38
arshia <arshiarezagholi1212@gmail.com>
2024-10-04 15:13:50 +0330

    completed the routes func and added the cors for it

--- broker/cmd/api/routes.go
+++ broker/cmd/api/routes.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "content-type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+		Debug:            true,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}

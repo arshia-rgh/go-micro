2fd046bd15d75ea8c5196df681671b89d23e0856
arshia <arshiarezagholi1212@gmail.com>
2024-10-05 21:44:06 +0330

    implemented the writeJSON file

--- broker/cmd/api/helpers.go
+++ broker/cmd/api/helpers.go
@@ -35,3 +35,28 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	return nil
 }
+
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+
+	out, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

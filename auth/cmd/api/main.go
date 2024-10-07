711a71967ba7b3b241a962c01711a6e7ff8a22cf
arshia <arshiarezagholi1212@gmail.com>
2024-10-07 18:12:36 +0330

    initialized an http.server

--- auth/cmd/api/main.go
+++ auth/cmd/api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"authentication/data"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 )
 
 const webPort = "8080"
@@ -15,6 +17,16 @@ type Config struct {
 
 func main() {
 	log.Println("Starting authentication service")
-	//app := Config{}
 
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%v", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }

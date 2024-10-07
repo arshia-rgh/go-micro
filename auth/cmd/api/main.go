00e64740f28ca7c44835b02937621d9d983350d7
arshia <arshiarezagholi1212@gmail.com>
2024-10-07 17:48:59 +0330

    added another field Models for config

--- auth/cmd/api/main.go
+++ auth/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "database/sql"
+import (
+	"authentication/data"
+	"database/sql"
+)
 
 const webPort = "8080"
 
 type Config struct {
-	DB *sql.DB
+	DB     *sql.DB
+	Models data.Models
 }
 
 func main() {

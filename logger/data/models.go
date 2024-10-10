3b49e7bb6b93896d73cd7afce5927d9988231048
arshia <arshiarezagholi1212@gmail.com>
2024-10-10 20:30:54 +0330

    implemented the LogEntry

--- logger/data/models.go
+++ logger/data/models.go
@@ -1,6 +1,9 @@
 package data
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
 
 var client *mongo.Client
 
@@ -16,4 +19,9 @@ type Models struct {
 }
 
 type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }

9eb7f24ae599a47d61d5026ee32a05cfab679d2d
arshia <arshiarezagholi1212@gmail.com>
2024-10-10 20:03:22 +0330

    implemented the func connectToMongo

--- logger/cmd/main/main.go
+++ logger/cmd/main/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
 )
 
 const (
@@ -28,4 +32,28 @@ func main() {
 
 func connectToMongo() (*mongo.Client, error) {
 
+	dbUser := os.Getenv("MONGO_USER")
+	dbPassword := os.Getenv("MONGO_PASSWORD")
+	dbHost := os.Getenv("MONGO_HOST")
+	dbPort := os.Getenv("MONGO_PORT")
+	dbName := os.Getenv("MONGO_DB")
+
+	uri := fmt.Sprintf("mongodb://%v:%v@%v:%v/%v", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	clientOptions := options.Client().ApplyURI(uri)
+
+	mongoClient, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = mongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB !")
+
+	return mongoClient, nil
 }

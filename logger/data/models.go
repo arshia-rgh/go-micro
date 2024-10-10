f68f674dc7a75b5d8f045592d8978ff30f106778
arshia <arshiarezagholi1212@gmail.com>
2024-10-10 20:20:48 +0330

    added the New func for models and also Models and LogENtry

--- logger/data/models.go
+++ logger/data/models.go
@@ -1,1 +1,19 @@
 package data
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{LogEntry: LogEntry{}}
+
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+}

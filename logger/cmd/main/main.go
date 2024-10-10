752335ecc32096f1cb05d6654f424f98f7e226a3
arshia <arshiarezagholi1212@gmail.com>
2024-10-10 19:09:05 +0330

    added connectToMongo func

--- logger/cmd/main/main.go
+++ logger/cmd/main/main.go
@@ -1,11 +1,31 @@
 package main
 
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
 const (
 	webPort  = "8080"
 	rpcPort  = "5001"
 	gRpcPort = "50001"
 )
 
+var client *mongo.Client
+
+type Config struct {
+}
+
 func main() {
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
 
 }

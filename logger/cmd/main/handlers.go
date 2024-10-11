415459dd33b82e0e4b60a7917af45fb8f3eba171
arshia <arshiarezagholi1212@gmail.com>
2024-10-11 23:18:48 +0330

    added the JSONPayload and the WriteLog handler

--- logger/cmd/main/handlers.go
+++ logger/cmd/main/handlers.go
@@ -1,1 +1,12 @@
 package main
+
+import "net/http"
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+
+}

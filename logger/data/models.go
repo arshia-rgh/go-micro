6e11acc7e7c0ba0cb743f99f86d6f2f0bd381abf
arshia <arshiarezagholi1212@gmail.com>
2024-10-11 16:19:10 +0330

    implemented the Insert func and made the db name globall

--- logger/data/models.go
+++ logger/data/models.go
@@ -1,14 +1,19 @@
 package data
 
 import (
+	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 	"time"
 )
 
 var client *mongo.Client
+var db *mongo.Database
 
-func New(mongo *mongo.Client) Models {
+func New(mongo *mongo.Client, dbName string) Models {
 	client = mongo
+	db = client.Database(dbName)
 
 	return Models{LogEntry: LogEntry{}}
 
@@ -24,4 +29,17 @@ type LogEntry struct {
 	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *LogEntry) Insert() (string, error) {
+	collection := db.Collection("logs")
+
+	one, err := collection.InsertOne(context.TODO(), l)
+	if err != nil {
+		log.Println("failed to insert into logs", err)
+		return "", err
+	}
+
+	return fmt.Sprint(one.InsertedID), nil
+
 }

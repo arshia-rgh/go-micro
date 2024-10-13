18cb769d5398e3ace6b8917291018f97da206019
arshia <arshiarezagholi1212@gmail.com>
2024-10-13 17:18:33 +0330

    added the mailMessage struct

--- mail/cmd/api/handlers.go
+++ mail/cmd/api/handlers.go
@@ -1,5 +1,12 @@
 package main
 
-func (app *Config) SendMail() {
+import "net/http"
 
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
 }

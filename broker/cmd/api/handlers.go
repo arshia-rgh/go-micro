daabc5db785e9be2d4f1e326cbd43502ef2c04a8
arshia <arshiarezagholi1212@gmail.com>
2024-10-13 21:09:36 +0330

    added the MailPaylaod and add field to the Request Payload in the broker

--- broker/cmd/api/handlers.go
+++ broker/cmd/api/handlers.go
@@ -12,6 +12,13 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
+}
+type MailPayload struct {
+	From    string `json:"from,omitempty"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 type AuthPayload struct {

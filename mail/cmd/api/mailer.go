4fb5bcb5a2dd0ce34588481c4e999ef8708fb278
arshia <arshiarezagholi1212@gmail.com>
2024-10-13 15:38:48 +0330

    implemented the Mail struct

--- mail/cmd/api/mailer.go
+++ mail/cmd/api/mailer.go
@@ -1,1 +1,15 @@
 package main
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        string
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+}
+
+type Message struct {
+}

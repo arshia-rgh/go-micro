e44b98f941a1cf928fefcb254b7054f5244d4674
arshia <arshiarezagholi1212@gmail.com>
2024-10-13 16:32:17 +0330

    added functions buildHtml and plain

--- mail/cmd/api/mailer.go
+++ mail/cmd/api/mailer.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"html/template"
+)
+
 type Mail struct {
 	Domain      string
 	Host        string
@@ -19,4 +24,66 @@ type Message struct {
 	Attachments []string
 	Data        any
 	DataMap     map[string]any
+}
+
+func (m *Mail) SendSMTPMessage(msg Message) error {
+	if msg.From == "" {
+		msg.From = m.FromAddress
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]any{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	formattedMessage, err := m.buildHTMLMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+}
+
+func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	templateToRender := "./templates/mail.html.gohtml"
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+
+	return formattedMessage, nil
+}
+
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	templateToRender := "./templates/mail.plain.gohtml"
+
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
 }

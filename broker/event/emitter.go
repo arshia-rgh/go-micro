5d4e338379cc3bf5daae751e086de2b4cdc44612
arshia <arshiarezagholi1212@gmail.com>
2024-10-14 22:42:09 +0330

    implemented the push method

--- broker/event/emitter.go
+++ broker/event/emitter.go
@@ -1,6 +1,8 @@
 package event
 
-import amqp "github.com/rabbitmq/amqp091-go"
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 type Emitter struct {
 	connection *amqp.Connection
@@ -16,4 +18,27 @@ func (emitter *Emitter) setup() error {
 	defer channel.Close()
 
 	return declareExchange(exchangeName, channel)
+}
+
+func (emitter *Emitter) push(event, key string) error {
+	channel, err := emitter.connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	err = channel.Publish(
+		exchangeName,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(event),
+		},
+	)
+
+	return err
 }

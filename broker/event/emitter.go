17a33077727287fd5a960744187b5d4f76ad0e59
arshia <arshiarezagholi1212@gmail.com>
2024-10-14 22:37:15 +0330

    added the setup function for the Emitter struct

--- broker/event/emitter.go
+++ broker/event/emitter.go
@@ -1,1 +1,19 @@
 package event
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type Emitter struct {
+	connection *amqp.Connection
+}
+
+func (emitter *Emitter) setup() error {
+	channel, err := emitter.connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	return declareExchange(exchangeName, channel)
+}

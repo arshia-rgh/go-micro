b705da1b312751edf8ebd8812fa6a98e3e460b55
arshia <arshiarezagholi1212@gmail.com>
2024-10-14 16:18:56 +0330

    added a constructor func for the Consumer struct

--- listener/event/consumers.go
+++ listener/event/consumers.go
@@ -6,3 +6,13 @@ type Consumer struct {
 	conn      *amqp.Connection
 	queueName string
 }
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+}
+
+func (consumer *Consumer) setup() error {
+
+}

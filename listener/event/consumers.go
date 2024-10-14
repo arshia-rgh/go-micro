ecff2ef2ca743d0a00baa0638dec38dd49b043c0
arshia <arshiarezagholi1212@gmail.com>
2024-10-14 16:40:21 +0330

    added the setup function for the consumer

--- listener/event/consumers.go
+++ listener/event/consumers.go
@@ -1,6 +1,9 @@
 package event
 
-import amqp "github.com/rabbitmq/amqp091-go"
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+)
 
 type Consumer struct {
 	conn      *amqp.Connection
@@ -14,5 +17,22 @@ func NewConsumer(conn *amqp.Connection) (Consumer, error) {
 }
 
 func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	exchangeName := os.Getenv("RABBITMQ_EXCHANGE")
+	return channel.ExchangeDeclare(
+		exchangeName,
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
 
 }

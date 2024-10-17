422aa7901d4016d3f9904705d318a0ce5417fbbb
arshia <arshiarezagholi1212@gmail.com>
2024-10-17 14:02:58 +0330

    added the LogInfo function

--- logger/cmd/api/rpc.go
+++ logger/cmd/api/rpc.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"log"
+	"logger/data"
+)
+
 type RPCServer struct{}
 
 type RPCPayload struct {
 	Name string
 	Data string
+}
+
+func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
+	logEntry := data.LogEntry{
+		Name: payload.Name,
+		Data: payload.Data,
+	}
+
+	_, err := logEntry.Insert()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	*resp = "Processed payload via RPC" + payload.Name
+	return nil
+
 }

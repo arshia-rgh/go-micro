94ac98e0bd2419650cf6aa86453b000db4e892f6
arshia <arshiarezagholi1212@gmail.com>
2024-10-17 17:42:16 +0330

    completed the Writelog function

--- logger/cmd/api/grpc.go
+++ logger/cmd/api/grpc.go
@@ -12,5 +12,23 @@ type LogServer struct {
 }
 
 func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
 
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	_, err := logEntry.Insert()
+
+	var res *logs.LogResponse
+	if err != nil {
+		res = &logs.LogResponse{Result: "failed to logg"}
+
+		return res, err
+	}
+
+	res = &logs.LogResponse{Result: "logged!"}
+
+	return res, nil
 }

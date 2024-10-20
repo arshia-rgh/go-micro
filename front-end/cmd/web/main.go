03e31ad7f5f45f821674977c94841444130a88df
arshia <arshiarezagholi1212@gmail.com>
2024-10-20 12:32:03 +0330

    minor changes to the front end

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
+var templateFS embed.FS
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 8082")
-	err := http.ListenAndServe(":8082", nil)
+	fmt.Println("Starting front end service on port 8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -22,19 +25,19 @@ func main() {
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return

5b379de3c9e75cd1827d325a41378dea94df99ad
arshia <arshiarezagholi1212@gmail.com>
2024-10-30 00:31:05 +0330

    completed Test_routes_exists

--- auth/cmd/api/routes_test.go
+++ auth/cmd/api/routes_test.go
@@ -10,14 +10,25 @@ func Test_routes_exists(t *testing.T) {
 	testApp := Config{}
 
 	testRoutes := testApp.routes()
-	chiRoutes := testRoutes.(chi.Router)
+	chiRoutes := testRoutes.(chi.Router) // actual routes
 
-	routes := []string{"/authenticate"}
+	routes := []string{"/authenticate"} // expected routes
+
+	for _, route := range routes {
+		routExists(t, chiRoutes, route)
+	}
 }
 
-func routExists(t *testing.T, routes chi.Router, router string) {
+func routExists(t *testing.T, routes chi.Router, route string) {
 	found := false
-	_ = chi.Walk(routes, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
-
+	_ = chi.Walk(routes, func(method string, foundRoute string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if route == foundRoute {
+			found = true
+		}
+		return nil
 	})
+
+	if !found {
+		t.Errorf("did not find %v in registered routes", route)
+	}
 }

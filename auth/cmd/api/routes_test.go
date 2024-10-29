704fa24eb9e8be972c19a05773fbc95e508b4ad0
arshia <arshiarezagholi1212@gmail.com>
2024-10-30 00:20:04 +0330

    added another routExists function

--- auth/cmd/api/routes_test.go
+++ auth/cmd/api/routes_test.go
@@ -1,7 +1,23 @@
 package main
 
-import "testing"
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"testing"
+)
 
 func Test_routes_exists(t *testing.T) {
 	testApp := Config{}
+
+	testRoutes := testApp.routes()
+	chiRoutes := testRoutes.(chi.Router)
+
+	routes := []string{"/authenticate"}
+}
+
+func routExists(t *testing.T, routes chi.Router, router string) {
+	found := false
+	_ = chi.Walk(routes, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+
+	})
 }

21ee4846b3cd3ecb6a57073151313d84820ec0ea
arshia <arshiarezagholi1212@gmail.com>
2024-10-30 10:47:16 +0330

    added NewPostgresTestRepository

--- auth/data/test-models.go
+++ auth/data/test-models.go
@@ -5,3 +5,9 @@ import "database/sql"
 type PostgresTestRepository struct {
 	Conn *sql.DB
 }
+
+func NewPostgresTestRepository(db *sql.DB) *PostgresTestRepository {
+	return &PostgresTestRepository{
+		Conn: db,
+	}
+}
